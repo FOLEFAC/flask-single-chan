b36c4b6c1305367ccded078a7758bfe8ab1a03c6
FOLEFAC <martinsderick99@gmail.com>
2024-10-11 08:46:09 +0100

    reduce workers and threads

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -17,11 +17,11 @@ import os
 
 
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '8'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '4'))
 
-threads = int(os.environ.get('GUNICORN_THREADS', '16'))
+threads = int(os.environ.get('GUNICORN_THREADS', '8'))
 
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+#timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5400')
 #gunicorn --config gunicorn.conf.py server:app
