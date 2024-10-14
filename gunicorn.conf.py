b292b8474dac94f623df6e13030c4e1d609593d4
FOLEFAC <martinsderick99@gmail.com>
2024-10-14 10:34:43 +0100

    added timeout

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -21,9 +21,9 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '4'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '8'))
 
-#timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5400')
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5000')
 #gunicorn --config gunicorn.conf.py server:app
 
 
